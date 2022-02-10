eae95c99793a23727962d37891f67d4ffee66c9b
UpSetSummerSet <72717739+14-TanKeeLiang@users.noreply.github.com>
2022-02-10 13:41:49 +0800

    Update <Taking Lab05 for reference to create our game>
    Taking Lab05 for reference to create our Obstacle Owen v2.0.

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private int coinCollected;
+    private float timer;
+
+    private AudioSource audioSource;
+
+    public AudioClip[] audioClips;
+    public Text coinCollectedTxt;
+    public Text timerTxt;
+
+    private int buildIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        buildIn = SceneManager.GetActiveScene().buildIndex;
+
+        if(buildIn == 2)
+        {
+            timer = 40;
+        }
+        if (buildIn == 3)
+        {
+            timer = 60;
+        }
+        if (buildIn == 4)
+        {
+            timer = 80;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coinCollected == 6 && buildIn == 2 || coinCollected == 10 && buildIn == 3 || coinCollected == 14 && buildIn == 4)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+        }
+
+        DisplayTime(timer);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin") && buildIn == 2)
+        {
+            audioSource.PlayOneShot(audioClips[0]);
+            coinCollected += 1;
+            coinCollectedTxt.text = "Coin Collected: " + coinCollected + "/6";
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("Coin") && buildIn == 3)
+        {
+            audioSource.PlayOneShot(audioClips[0]);
+            coinCollected += 1;
+            coinCollectedTxt.text = "Coin Collected: " + coinCollected + "/10";
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("Coin") && buildIn == 4)
+        {
+            audioSource.PlayOneShot(audioClips[0]);
+            coinCollected += 1;
+            coinCollectedTxt.text = "Coin Collected: " + coinCollected + "/14";
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("Water"))
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerTxt.text = "Time Left: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
---
+++ Assets/Standard Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    private static int currentLevel = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void PlayBtn()
+    {
+        SceneManager.LoadScene("LevelSelection");
+    }
+
+    public void EasyLevel()
+    {
+        currentLevel = 1;
+        SceneManager.LoadScene("EasyLevel");
+    }
+
+    public void MediumLevel()
+    {
+        currentLevel = 2;
+        SceneManager.LoadScene("MediumLevel");
+    }
+
+    public void HardLevel()
+    {
+        currentLevel = 3;
+        SceneManager.LoadScene("HardLevel");
+    }
+
+    public void RestartBtn()
+    {
+        if (currentLevel == 1)
+        {
+            SceneManager.LoadScene("EasyLevel");
+        }
+        if (currentLevel == 2)
+        {
+            SceneManager.LoadScene("MediumLevel");
+        }
+        if (currentLevel == 3)
+        {
+            SceneManager.LoadScene("HardLevel");
+        }
+    }
+
+    public void MainMenuBtn()
+    {
+        currentLevel = 0;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitBtn()
+    {
+        Application.Quit();
+    }
+}
